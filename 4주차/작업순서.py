d69f429b5e6ae41d309877b43ba9f7341493429f
GaYoung87 <gyyoon4u@naver.com>
2019-08-20 16:40:52 +0900

    Study Stack Algorithm
     - DFS
     - 작업순서
     - ladder
     - Stack 연습문제

--- 4주차/작업순서.py
+++ 4주차/작업순서.py
@@ -1,4 +1,4 @@
-for t in range(1):
+for t in range(10):
     v, e = map(int, input().split())
     data = list(map(int, input().split()))
     adj = [[] for i in range(v + 1)]
@@ -10,4 +10,16 @@ for t in range(1):
     # print(adj)    # [[], [2, 5], [3], [], [1], []]
     # print(visit)  # [None, 1, 1, 1, 0, 1]
 
-    for i in 
+    result = ''
+    for i in range(1, v+1):
+        stack = []
+        stack.append(i)
+        while stack:
+            node = stack.pop()
+            if visit[node] > 0:
+                visit[node] -= 1
+            elif visit[node] == 0:
+                visit[node] = 'X'
+                result += str(node) + ' '
+                stack.extend(adj[node])
+    print('#{} {}'.format(t+1, result))
