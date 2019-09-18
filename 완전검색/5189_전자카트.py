0623f12cbb823412078d7b637d0b0e88b11f54bf
GaYoung87 <gyyoon4u@naver.com>
2019-09-18 20:00:11 +0900

    Algo prob 0918

--- 완전검색/5189_전자카트.py
+++ 완전검색/5189_전자카트.py
@@ -1,15 +1,17 @@
+def cart():
+
+
+
 for t in range(int(input())):
     N = int(input())
     board = [list(map(int, input().split()))]
     visit = [True] + [False] * (N - 1)
 
-    mymin = 9999
+    mymin = 9999999999
     for i in range(N):
         for j in range(N):
-            if board[i][j] != 0:
-                result = board[i][j]
-
+            if board[0][j] != 0:
+                result = board[0][j]
                 visit[j] = True
                 if j == i:
-
 
