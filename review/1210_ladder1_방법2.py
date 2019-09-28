7596b056c1ff367958ae1ffe1769e6c3cd053768
GaYoung87 <gyyoon4u@naver.com>
2019-09-28 23:43:07 +0900

    Algo review + 개념(재귀) 0928

--- review/1210_ladder1_방법2.py
+++ review/1210_ladder1_방법2.py
@@ -1,10 +1,16 @@
-for t in range(10):
+for t in range(1):
     N = int(input())
-    board = [list(map(int, input().split())) for _ in range(100)]
+    board = [list(map(int, input().split())) for _ in range(10)]
 
-    j = board[99].index(2)
+    j = board[9].index(2)  # y값
 
-    for i in range(99, 0 , -1):
-        if j != 0 and board[i][j-1]:
-            while j > 0 and board[i][j-1]:
+    for i in range(9, 0, -1):  # x값
+        if j != 0 and board[i][j-1] == 1:
+            while j > 0 and board[i][j-1] == 1:
                 j -= 1
+
+        elif j != 9 and board[i][j+1] == 1:
+            while j < 9 and board[i][j+1] == 1:
+                j += 1
+
+    print(j)
---
+++ review/1211_ladder2_방법1.py
@@ -0,0 +1,5 @@
+for t in range(int(input())):
+    N = 10
+    board = [list(map(int, input().split())) for _ in range(9)]
+
+    for i in range(10):
