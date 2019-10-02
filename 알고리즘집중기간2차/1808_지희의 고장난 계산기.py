1c930c3a507cce64afcfef0fa378b8d429231e57
GaYoung87 <gyyoon4u@naver.com>
2019-10-02 15:12:18 +0900

    1002

---
+++ 알고리즘집중기간2차/1808_지희의 고장난 계산기.py
@@ -0,0 +1,10 @@
+for t in range(int(input())):
+    data = list(map(int, input().split()))
+    final = int(input())
+    num = []   # [1, 2, 5]
+
+    for i in range(10):
+        if data[i] == 1:
+            num.append(i)
+
+
