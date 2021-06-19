3f616847a92466bbdddd7ddd19cf5431ff740511
Gayoung <gyyoon4u@naver.com>
2021-06-19 22:10:35 +0900

    0619

---
+++ 2021_algo/프로그래머스/20210619_다리를 지나는 트럭.py
@@ -0,0 +1,13 @@
+def solution(bridge_length, weight, truck_weights):
+    answer = 0
+    return answer
+
+bridge_length = 2
+weight = 10
+truck_weights = [7,4,5,6]  # 8
+# bridge_length = 100
+# weight = 100
+# truck_weights = [10]  # 101
+# bridge_length = 100
+# weight = 100
+# truck_weights = [10,10,10,10,10,10,10,10,10,10]  # 110
