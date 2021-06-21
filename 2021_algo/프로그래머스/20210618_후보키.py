3d1233a9e61a9d693bde0f75211e8df17c0420ae
Gayoung <gyyoon4u@naver.com>
2021-06-21 23:18:28 +0900

    0621

--- 2021_algo/프로그래머스/20210618_후보키.py
+++ 2021_algo/프로그래머스/20210618_후보키.py
@@ -1,6 +1,30 @@
+from itertools import combinations
+
 def solution(relation):
-    answer = 0
-    return answer
+
+    total = []
+    y = len(relation[0])
+    for i in range(1, y + 1):
+        total.extend(combinations(range(y), i))
+
+    # 유일성
+    unique = []
+    for t in total:
+        temp = []
+        for rel in relation:
+            temp.append(tuple([rel[i] for i in t]))
+        
+        if len(set(temp)) == len(relation):
+            unique.append(t)
+
+    # 최소성
+    answer = set(unique)
+    for i in range(len(unique)-1):
+        for j in range(i+1, len(unique)):
+            if len(set(unique[i])) == len(set(unique[i]) & set(unique[j])):
+                answer.discard(unique[j])
+
+    return len(answer)
 
 
 relation = [["100","ryan","music","2"],
@@ -9,13 +33,3 @@ relation = [["100","ryan","music","2"],
             ["400","con","computer","4"],
             ["500","muzi","music","3"],
             ["600","apeach","music","2"]]
-
-from itertools import combinations
-
-total = []
-y = len(relation[0])
-for i in range(1, y + 1):
-    total.extend(combinations(range(y), i))
-print(total)
-
-
--- 2021_algo/프로그래머스/20210620_배달.py
+++ 2021_algo/프로그래머스/20210620_배달.py
@@ -1,7 +1,33 @@
-def solution(N, road, K):
-    answer = 0
+from collections import deque
 
-    return answer
+def solution(N, road, K):
+    nodes = {}
+    dist = { i:float('inf') if i != 1 else 0 for i in range(1, N+1) }
+    for v1, v2, d in road:
+        if v1 in nodes:
+            nodes[v1].append((v2, d))
+        else:
+            nodes[v1] = [(v2, d)]
+        if v2 in nodes:
+            nodes[v2].append((v1, d))
+        else:
+            nodes[v2] = [(v1, d)]
+
+    q = deque([1])
+    while q:
+        now_node = q.popleft()
+        for next_node, d in nodes[now_node]:
+            if dist[next_node] > dist[now_node] + d:
+                dist[next_node] = dist[now_node] + d
+                q.append(next_node)
+
+    cnt = 0
+    for key, d in dist.items():
+        if d <= K:
+            cnt += 1
+    # print(cnt)
+
+    return cnt
 
 N = 5
 road = [[1,2,1],[2,3,3],[5,2,2],[1,4,2],[5,3,1],[5,4,2]]
@@ -10,13 +36,8 @@ K = 3  # 4
 # road = [[1,2,1],[1,3,2],[2,3,2],[3,4,3],[3,5,2],[3,5,3],[5,6,1]]
 # K = 4  # 4
 
-from collections import deque
 
+# {1: [(2, 1), (4, 2)], 2: [(1, 1), (3, 3), (5, 2)],
+#  3: [(2, 3), (5, 1)], 5: [(2, 2), (3, 1), (4, 2)], 
+#  4: [(1, 2), (5, 2)]}
 
-nodes = {}
-dist = { i:float('inf') for i in range(1, N+1) }
-for v1, v2, d in road:
-    nodes[v1] = nodes.get(v1, []) + [(v2, d)]
-    nodes[v2] = nodes.get(v2, []) + [(v1, d)]
-que = deque([1])
-print(nodes)                                                                                                                                               
