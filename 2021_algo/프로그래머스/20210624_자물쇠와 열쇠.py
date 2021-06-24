a9fdde977c44f4d3f8d2c6ec9f3bf82beba9d54c
Gayoung <gyyoon4u@naver.com>
2021-06-24 23:55:25 +0900

    0624

---
+++ 2021_algo/프로그래머스/20210624_자물쇠와 열쇠.py
@@ -0,0 +1,11 @@
+def solution(key, lock):
+    answer = True
+    return answer
+
+key = [[0, 0, 0], [1, 0, 0], [0, 1, 1]]
+lock = [[1, 1, 1], [1, 1, 0], [1, 0, 1]]  # true
+
+len_key = len(key)
+len_lock = len(lock)
+
+board = [[0] * ()]
