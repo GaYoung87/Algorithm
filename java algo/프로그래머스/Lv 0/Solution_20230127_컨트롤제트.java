21861b0404ff772b8b1da856f8c9e0589ba9f11c
Gayoung <gyyoon4u@naver.com>
2023-01-30 00:20:00 +0900

    20230127_컨트롤제트

---
+++ java algo/프로그래머스/Lv 0/Solution_20230127_컨트롤제트.java
@@ -0,0 +1,20 @@
+public class Solution_20230127_컨트롤제트 {
+
+    public int solution(String s) {
+
+        int answer = 0;
+        String[] array = s.split(" ");
+        for (int i=0; i<array.length; i++) {
+            if (array[i].equals("Z")) {
+                array[i-1] = "0";
+                array[i] = "0";
+
+            }
+        }
+
+        for (int i=0; i<array.length; i++) {
+            answer += Integer.parseInt(array[i]);
+        }
+        return answer;
+    }
+}
