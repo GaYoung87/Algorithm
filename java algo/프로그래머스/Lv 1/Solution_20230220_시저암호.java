d8cb78d64d5433b562a9f7c408fb3ff26ec4fc82
Gayoung <gyyoon4u@naver.com>
2023-02-21 00:34:59 +0900

    20230220_시저암호

--- java algo/프로그래머스/Lv 1/Solution_20230220_시저암호.java
+++ java algo/프로그래머스/Lv 1/Solution_20230220_시저암호.java
@@ -1,15 +1,46 @@
 public class Solution_20230220_시저암호 {
 
+    /**
+     * @param s
+     * @param n
+     * @return
+     *
+     * char 와 int간의 형변환 https://godsu94.tistory.com/167
+     * [ 진행방법 ]
+     * CHAR의 경우에는 숫자와 더하면 숫자로 나오고, 이를 (char)로 변환해주면 char로 바뀜
+     * z의 경우 +1이 되면 a로 가야하므로 26(문자 a~z갯수)을 빼준다
+     * Z도 동일
+     */
     public String solution(String s, int n) {
         String answer = "";
-        String[] ss = s.split("");
 
-        for (int i=0; i<ss.length; i++) {
-            String word = ss[i];
-            System.out.println("word.charAt() = " + word.charAt());
+        for (int i=0; i<s.length(); i++) {
+            char word = s.charAt(i);
 
-            if (word.equals(" ")) {
+            if (word == ' ') {
                 answer += word;
+            }
+
+            // 소문자
+            if (word >= 'a' && word <= 'z') {
+                if (word + n > 'z') {
+                    answer += (char)(word-26+n);
+                    System.out.println("word+n = " + (char)(word-26+n));
+                } else {
+                    answer += (char)(word+n);
+                    System.out.println("word+n = " + (char)(word+n));
+                }
+            }
+
+            // 대문자
+            if (word >= 'A' && word <= 'Z') {
+                if (word + n > 'Z') {
+                    answer += (char)(word-26+n);
+                    System.out.println("word+n = " + (char)(word-26+n));
+                } else {
+                    answer += (char)(word+n);
+                    System.out.println("word+n = " + (char)(word+n));
+                }
             }
 
         }
