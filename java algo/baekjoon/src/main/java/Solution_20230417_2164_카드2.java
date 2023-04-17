b0ca5eafae4e46c1c7ec2f8ba7ad3db2de273d73
Gayoung <gyyoon4u@naver.com>
2023-04-17 23:20:25 +0900

    20230417_카드2

---
+++ java algo/baekjoon/src/main/java/Solution_20230417_2164_카드2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Solution_20230417_2164_카드2 {
+
+    static int[] q;
+
+    static int size = 0;
+    static int front = 0;
+    static int back = 0;
+
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+
+        q = new int[N];
+    }
+}
